2491d1d45353a726b2e97b251168d62a0452df03
Liam <LCCoombs@hotmail.co.uk>
2021-07-02 22:32:51 +0100

    Add devtools

--- models/user.go
+++ models/user.go
@@ -2,8 +2,10 @@ package model
 
 type (
 	User struct {
-		Name     string `json:"name" gorm:"primaryKey"`
-		Email    string `json:"email"`
-		Password string `json:"password"`
+		Name            string `json:"name" gorm:"primaryKey"`
+		Email           string `json:"email"`
+		Password        string `json:"password"`
+		User_Group      string `json:"user_group"`
+		Workspace_Group string `json:"workspace_group"`
 	}
 )
