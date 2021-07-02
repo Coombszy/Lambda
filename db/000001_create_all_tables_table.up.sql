cd2a34ad7fea9bbad3cdcb172964cbdfe3f59d14
Liam <LCCoombs@hotmail.co.uk>
2021-07-02 20:13:13 +0100

    Change to use correct brackets

--- db/000001_create_all_tables_table.up.sql
+++ db/000001_create_all_tables_table.up.sql
@@ -3,7 +3,7 @@
 -------------------------
 -- USER TABLES
 -------------------------
-CREATE TABLE IF NOT EXISTS users(
+CREATE TABLE IF NOT EXISTS users (
    id SERIAL PRIMARY KEY,
    name VARCHAR (32) NOT NULL,
    email VARCHAR(64) UNIQUE NOT NULL,
@@ -12,36 +12,36 @@ CREATE TABLE IF NOT EXISTS users(
    last_login DATETIME
 );
 
-CREATE TABLE IF NOT EXISTS user_groups(
+CREATE TABLE IF NOT EXISTS user_groups (
    id SERIAL PRIMARY KEY,
    name VARCHAR (16) NOT NULL
-)
+);
 
-CREATE TABLE IF NOT EXISTS user_group_assignment {
+CREATE TABLE IF NOT EXISTS user_group_assignment (
    id SERIAL PRIMARY KEY,
    user_id SERIAL REFERENCES users(id),
    group_id SERIAL REFERENCES user_groups(id)
-}
+);
 
 -------------------------
 -- WORKSPACE TABLES
 -------------------------
-CREATE TABLE IF NOT EXISTS workspaces {
+CREATE TABLE IF NOT EXISTS workspaces (
    id SERIAL PRIMARY KEY,
    name VARCHAR(32),
    creation DATETIME NOT NULL,
    last_used DATETIME NOT NULL,
    floors INT NOT NULL
-}
+);
 
-CREATE TABLE IF NOT EXISTS workspace_groups {
+CREATE TABLE IF NOT EXISTS workspace_groups (
    id SERIAL PRIMARY KEY,
    name VARCHAR(16) NOT NULL,
    workspace_id SERIAL REFERENCES workspaces(id),
    user_id SERIAL REFERENCES users(id)
-}
+);
 
-CREATE TABLE IF NOT EXISTS desks {
+CREATE TABLE IF NOT EXISTS desks (
    id SERIAL PRIMARY KEY,
    name VARCHAR(16) NOT NULL,
    creation DATETIME NOT NULL,
@@ -50,12 +50,12 @@ CREATE TABLE IF NOT EXISTS desks {
    location_x INT NOT NULL,
    location_y INT NOT NULL,
    updated_by SERIAL REFERENCES users(id)
-}
+);
 
-CREATE TABLE IF NOT EXISTS desk_bookings {
+CREATE TABLE IF NOT EXISTS desk_bookings (
    id SERIAL PRIMARY KEY,
    desk_id SERIAL REFERENCES desks(id),
    user_id SERIAL REFERENCES users(id),
    start_date DATETIME NOT NULL,
    end_date DATETIME
-}
+);
